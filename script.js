72d6ec726922e64c06b66c95fad9c3dde94fb9f0
eduardojigub <eduardobrunoit@gmail.com>
2023-01-22 14:06:13 -0300

    feat: add javascript logic, local storage and functions

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+//selectors
+
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-};
+//Event Listeners
+button.addEventListener("click", registerMyDay);
+form.addEventListener("change", save);
 
+//functions
+function registerMyDay() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    return alert("Dia já incluso 🔴");
+  }
+  alert("Adicionado com sucesso ✅");
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
